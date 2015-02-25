279d212e6a190f6d96e95df7d07b3fd4ac544fb5
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-24 20:31:57 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -6,8 +6,12 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 class RegisterForm(Form):
       #profile_photo =FileField('Profile Image')
 	userID =IntegerField('ID:', [Required()])
-	upload = FileField('image', validators=[FileRequired(),FileAllowed(images, 'Images only!')])
+	username = TextField('Username:', [Required()])
+	img = FileField('image', validators=[FileRequired(),FileAllowed(images, 'Images only!')])
 	firstName = TextField('First Name:', [Required()])
 	lastName = TextdField('Last Name:', [Required()])
 	sex = SelectField('Sex', choices=[('M','Male'),('F''Female')] default = 'M', [Required()])
 	age = IntegerField('Age:', [Required()])
+	profile_add_on = DateField('Profile Created',[validators.required()])
+	high_score = IntegerField('High Score', [Optional()])
+	tdollars = IntegerField('TDollars', [Optional()])
