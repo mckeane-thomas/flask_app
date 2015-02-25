072fececeea56eb07832ab6aede1bd4412a8cb0b
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-24 20:14:11 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@
     from wtforms import TextField, IntegerField, DateField
     from wtforms.validators import Required, Optional, FileField, file_required,
 
-  class ProfileForm(Form):
+  class RegisterForm(Form):
       #profile_photo =FileField('Profile Image')
 	userID =IntegerField('ID:', [Required()])
 	firstName = TextField('First Name:', [Required()])
